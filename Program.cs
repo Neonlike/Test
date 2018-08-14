0c40ca1ccfe063e76e245216f30793f41ba12ddd
Vlad Koloshych <neonlike98@gmail.com>
2018-08-14 18:35:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Hello 3");
+            Console.ReadKey();
+        }
+    }
+}
